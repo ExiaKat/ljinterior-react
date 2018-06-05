97ff3e850d8e47c878afa28e6579d15847b211c2
Hongfeng Xia <nemo_xia@yahoo.com.au>
2018-06-05 21:21:24 +0800

    Add polyfill for IE11 support

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { ParallaxProvider } from 'react-scroll-parallax';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'babel-polyfill';
 
 ReactDOM.render(<ParallaxProvider><App /></ParallaxProvider>, document.getElementById('root'));
 registerServiceWorker();
